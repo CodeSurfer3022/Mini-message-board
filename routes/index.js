0442f6b74dd1c4ebfc225bff35bb9d414ab095ed
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-11-19 10:03:34 +0530

    Add sample messages to index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there",
+    user: "Rama",
+    added: new Date()
+  },
+  {
+    text: "Hello World",
+    user: "Chakri",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message board', messages: messages });
 });
 
 module.exports = router;
