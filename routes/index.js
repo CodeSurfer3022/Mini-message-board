479bede09b1be0150589b30af4ddaf375cc25855
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-11-19 14:29:51 +0530

    change view to pug

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
-const messages = [
-  {
-    text: "Hi there",
-    user: "Rama",
-    added: new Date()
-  },
-  {
-    text: "Hello World",
-    user: "Chakri",
-    added: new Date()
-  }
-];
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Message board', messages: messages });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
