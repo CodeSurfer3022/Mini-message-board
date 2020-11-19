84b7872655c11fecbc6384e3f88a13af5e5413b7
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-11-19 15:07:52 +0530

    Add form view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Messages', messages: messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', {title: 'Add a new message'})
 });
 
 module.exports = router;
